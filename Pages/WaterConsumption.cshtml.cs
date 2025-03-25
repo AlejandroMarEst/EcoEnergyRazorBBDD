cffbcc8b8e1700fd3c367de8dec19f66c9c93b38
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-03-25 12:29:32 +0100

    Revert "Adapted Show Water Consumption page to use BBDD"
    This reverts commit ad51cbfb2104abcdad1a0b87950d84722cd80052.

--- Pages/WaterConsumption.cshtml.cs
+++ Pages/WaterConsumption.cshtml.cs
@@ -1,6 +1,5 @@
 using CsvHelper;
 using CsvHelper.Configuration;
-using EcoEnergyRazorBBDD.Data;
 using EcoEnergyRazorBBDD.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,14 +11,38 @@ namespace EcoEnergyRazorBBDD.Pages
     public class WaterConsumptionModel : PageModel
     {
         public List<WaterConsumptionLog> WaterConsumptionList { get; set; } = new List<WaterConsumptionLog>();
-        public bool FileHasRecords { get; set; } = true;
+        public bool Exists { get; set; } = true;
+        public bool HasContent { get; set; } = true;
 
         // Handles GET request to load water consumption data
         public void OnGet()
         {
-            using var context = new ApplicationDbContext();
-            WaterConsumptionList = context.WaterConsumption.ToList();
-            FileHasRecords = WaterConsumptionList.Count > 0;
+            const string filePath = "wwwroot/Files/consum_aigua_cat_per_comarques.csv";
+
+            // Check if the file exists
+            if (System.IO.File.Exists(filePath))
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HasHeaderRecord = true,
+                };
+                using var reader = new StreamReader(filePath);
+                using (var csv = new CsvReader(reader, config))
+                {
+                    // Read water consumption records from CSV file
+                    var records = csv.GetRecords<WaterConsumptionLog>();
+                    foreach (var line in records)
+                    {
+                        WaterConsumptionList.Add(line);
+                    }
+                    // Check if the file has any content
+                    HasContent = WaterConsumptionList.Count == 0;
+                }
+            }
+            else
+            {
+                Exists = false;
+            }
         }
     }
 }
